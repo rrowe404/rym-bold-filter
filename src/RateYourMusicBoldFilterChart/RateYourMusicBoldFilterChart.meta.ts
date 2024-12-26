2abcc2fd79f395bf6668252fc3f1d6fa1f8bf682
rrowe404 <140030215+rrowe404@users.noreply.github.com>
2024-12-25 22:54:47 -0800

    hmm, this should have already been committed

--- src/RateYourMusicBoldFilterChart/RateYourMusicBoldFilterChart.meta.ts
+++ src/RateYourMusicBoldFilterChart/RateYourMusicBoldFilterChart.meta.ts
@@ -3,7 +3,7 @@
 import { Metadata } from "userscript-metadata-generator";
 
 const metadata: Metadata = {
-    name: 'Rate Your Music bold filter',
+    name: 'Rate Your Music Chart bold filter',
     description: 'Adds a checkbox to RYM chart pages to show only bolded works',
     match: 'https://rateyourmusic.com/charts/*',
     grant: ['GM.getValue', 'GM.setValue']
